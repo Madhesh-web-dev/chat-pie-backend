22a84e0d05816a1164d293fa5b63818ab27e8157
Madhesh B <madhesh5463@gmail.com>
2025-10-18 18:52:19 +0530

    cors origin

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://chat-pie.netlify.app/', credentials: true }));
 app.use(express.json());
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://chat-pie.netlify.app',
     methods: ['GET', 'POST'],
     credentials: true,
   },
